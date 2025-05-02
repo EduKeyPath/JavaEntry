b11fded8ba1dd926dda6cd505dabfbb703b49ff8
Banti Shaw <bantishaw@Bantis-Air.bbrouter>
2025-05-02 12:18:40 +0530

    abstract class, modifers, encapsulation

---
+++ ClassModifiersEncapEx12.java
@@ -0,0 +1,13 @@
+package JavaEntry;
+
+public class ClassModifiersEncapEx12 {
+    private String fullName = "Test";
+
+    public String getName(){ // Getter Method
+        return fullName;
+    }
+
+    public void setName(String newName){  // Setter Method
+        this.fullName = newName;
+    }
+}
